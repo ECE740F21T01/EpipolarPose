2b5942d780e5a45bbfdb25a6ca101f8db4c22cab
Liyao Jiang <liyao1@ualberta.ca>
2021-10-13 22:59:57 -0600

    adding h36m video2image script and S1 corrections

---
+++ data/h36m/recover_S1_mismatch.sh
@@ -0,0 +1,17 @@
+mv Images/S1/Photo.54138969 Images/S1/TakingPhoto.54138969
+mv Images/S1/Photo.55011271 Images/S1/TakingPhoto.55011271
+mv Images/S1/Photo.58860488 Images/S1/TakingPhoto.58860488
+mv Images/S1/Photo.60457274 Images/S1/TakingPhoto.60457274
+mv Images/S1/Photo_1.54138969 Images/S1/TakingPhoto_1.54138969
+mv Images/S1/Photo_1.55011271 Images/S1/TakingPhoto_1.55011271
+mv Images/S1/Photo_1.58860488 Images/S1/TakingPhoto_1.58860488
+mv Images/S1/Photo_1.60457274 Images/S1/TakingPhoto_1.60457274
+
+mv Images/S1/WalkDog.54138969 Images/S1/WalkingDog.54138969
+mv Images/S1/WalkDog.55011271 Images/S1/WalkingDog.55011271
+mv Images/S1/WalkDog.58860488 Images/S1/WalkingDog.58860488
+mv Images/S1/WalkDog.60457274 Images/S1/WalkingDog.60457274
+mv Images/S1/WalkDog_1.54138969 Images/S1/WalkingDog_1.54138969
+mv Images/S1/WalkDog_1.55011271 Images/S1/WalkingDog_1.55011271
+mv Images/S1/WalkDog_1.58860488 Images/S1/WalkingDog_1.58860488
+mv Images/S1/WalkDog_1.60457274 Images/S1/WalkingDog_1.60457274
